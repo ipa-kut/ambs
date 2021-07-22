81429740ae6c7efa10fac4c46bf3a25140d90b95
kut <kut@ubuntu.p52.ipa>
2021-07-22 16:38:50 +0200

    add odom location interpreter

--- ambs_components/include/ambs_interpreters/location_interpreters/odom_interpreter.h
+++ ambs_components/include/ambs_interpreters/location_interpreters/odom_interpreter.h
@@ -1,11 +1,49 @@
-#ifndef ODOM_INTERPRETER_H
-#define ODOM_INTERPRETER_H
+#ifndef AMBS_INTERPRETERS_LOCATION_INTERPRETERS_ODOM_INTERPRETER_H
+#define AMBS_INTERPRETERS_LOCATION_INTERPRETERS_ODOM_INTERPRETER_H
 
+#include <string>
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/Float64.h>
 
+#include "ambs_msgs/BoolStamped.h"
+#include "ambs_base_interface/ambs_boolean_interface.hpp"
+
+namespace ambs_interpreters {
+
+/**
+ * @brief Used primarily in simulation testing environment instead of focusers with topic_tools/transform to
+ * extract Pose of robot and Linear X velocity.
+ * Advantage is potential to publish odom data from a nodelet for testing rather than taking from gazebo.
+ *
+ * Ports:
+ * Inputs - Odom
+ * Outputs - Pose_stamped, twist_linear_x
+ */
 class OdomInterpreter
 {
 public:
   OdomInterpreter();
+  OdomInterpreter(ros::NodeHandle nh, std::string node_name);
+  void init();
+
+private:
+  ros::NodeHandle nh_;
+  ros::Publisher pub_pose_;
+  ros::Publisher pub_lin_x_;
+  ros::Subscriber sub_odom_;
+  std::string node_name_;
+
+  const std::string ODOM_ = "odom";
+  const std::string POSE_STAMPED_ = "pose";
+  const std::string TWIST_LINEARX_ = "twist_linear_x";
+
+  const unsigned int queue_size = 10;
+
+  void odomCB(const nav_msgs::OdometryConstPtr msg);
 };
 
-#endif // ODOM_INTERPRETER_H
+}  // namespace ambs_interpreters
+
+#endif  // AMBS_INTERPRETERS_LOCATION_INTERPRETERS_ODOM_INTERPRETER_H
---
+++ ambs_pluginlib/src/interpreters/location_interpreters/odom_interpreter_nodelet.cpp
@@ -0,0 +1,34 @@
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+#include <string>
+
+#include "ambs_interpreters/location_interpreters/odom_interpreter.h"
+
+namespace ambs_interpreters
+{
+
+class OdomInterpreterNodelet : public nodelet::Nodelet
+{
+public:
+  OdomInterpreterNodelet() {}
+  ~OdomInterpreterNodelet() {}
+
+  virtual void onInit()
+      {
+        ros::NodeHandle nh = this->getMTPrivateNodeHandle();
+        std::string name = nh.getUnresolvedNamespace();
+        name = name.substr(name.find_last_of('/') + 1);
+
+        NODELET_INFO_STREAM("Initialising nodelet... [" << name << "]");
+        class_object.reset(new OdomInterpreter(nh, name));
+        class_object->init();
+      }
+
+private:
+    boost::shared_ptr<OdomInterpreter> class_object;
+};
+
+}  // namespace ambs_interpreters
+
+PLUGINLIB_EXPORT_CLASS(ambs_interpreters::OdomInterpreterNodelet,
+                       nodelet::Nodelet);
